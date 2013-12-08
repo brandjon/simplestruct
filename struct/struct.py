67dc21fb1c3ca8c68b962f205ae0aca8bf1841da
Jon Brandvein <jon.brandvein@gmail.com>
2013-12-07 22:40:42 -0500

    Added Struct.

---
+++ struct/struct.py
@@ -0,0 +1,165 @@
+"""Provides field descriptors and the base and meta classes for struct."""
+
+
+__all__ = [
+    'Field',
+    'MetaStruct',
+    'Struct',
+]
+
+
+from collections import OrderedDict
+
+from struct.type import check_spec, normalize_kind, normalize_mods
+
+
+class Field:
+    
+    """Descriptor for declaring struct fields. Fields have a name
+    and type spec (kind and mods). In addition to those specified
+    in types.py, mods may include:
+    
+        '!': this field is derived data that should not be passed as
+            an argument to the struct's constructor or consulted for
+            equality/hashing
+    
+    All writes are type-checked according to the type spec. 
+    Writing to a field will fail with AttributeError if the struct
+    is immutable and has finished initializing.
+    """
+    
+    # TODO: It would be a pretty sweet/evil metacircularity to define
+    # Field itself as a Struct.
+    
+    # The attribute "name" is assigned by MetaStruct.
+    
+    def __init__(self, kind, mods=()):
+        self.kind = normalize_kind(kind)
+        self.mods = normalize_mods(mods)
+    
+    def __get__(self, inst, value):
+        if inst is None:
+            raise AttributeError('Cannot retrieve field from class')
+        return inst.__dict__[self.name]
+    
+    def __set__(self, inst, value):
+        if inst._immutable and inst._initialized:
+            raise AttributeError('Struct is immutable')
+        
+        check_spec(value, self.kind, self.mods)
+        
+        inst.__dict__[self.name] = value
+
+
+class MetaStruct(type):
+    
+    """Metaclass for Structs.
+    
+    Upon class definition (of a new Struct subtype), set class attribute
+    _fields to be a tuple of the Field descriptors, in declaration
+    order.
+    
+    Upon instantiation of a Struct subtype, set its _initialized
+    attribute to True after __init__() returns.
+    """
+    
+    @property
+    def _primary_fields(cls):
+        """Non-derived fields, i.e. fields that don't have a '!'
+        modified.
+        """
+        return [f for f in cls._fields if '!' not in f.mods]
+    
+    # Use OrderedDict to preserve Field declaration order.
+    @classmethod
+    def __prepare__(mcls, name, bases, **kwds):
+        return OrderedDict()
+    
+    # Construct the _fields attribute on the new class.
+    def __new__(mcls, clsname, bases, namespace, **kwds):
+        fields = []
+        for fname, f in namespace.copy().items():
+            if not isinstance(f, Field):
+                continue
+            
+            f.name = fname
+            fields.append(f)
+        
+        cls = super().__new__(mcls, clsname, bases, dict(namespace), **kwds)
+        
+        cls._fields = tuple(fields)
+        
+        return cls
+    
+    # Mark the class as _initialized after construction.
+    def __call__(mcls, *args, **kargs):
+        inst = super().__call__(*args, **kargs)
+        inst._initialized = True
+        return inst
+
+
+class Struct(metaclass=MetaStruct):
+    
+    """A fixed structure class that supports default constructors,
+    type-checking and coersion, immutable fields, pretty-printing,
+    equality, and hashing.
+    
+    By default, __new__() will initialize the non-derived fields.
+    If immutable, fields may still be written to until __init__()
+    (of the last subclass) returns.
+    """
+    
+    @property
+    def _primary_fields(self):
+        return self.__class__._primary_fields
+    
+    _immutable = True
+    """Flag for whether to allow reassignment to fields after
+    construction. Override with False in subclass to allow.
+    """
+    
+    # TODO: Allow keyword arguments to constructor.
+    # Might be implemented using inspect.Signature.
+    
+    # We expect there to be one constructor argument for each
+    # non-derived field (i.e. a field without the '!' modifier),
+    # in field declaration order.
+    def __new__(cls, *args):
+        inst = super().__new__(cls)
+        inst._initialized = False
+        
+        pfields = cls._primary_fields
+        
+        if not len(args) == len(pfields):
+            raise ValueError('{} expects {} args, got {}'.format(
+                             cls.__name__, len(pfields), len(args)))
+        
+        # TODO: better error message here if typecheck fails
+        for f, arg in zip(pfields, args):
+            setattr(inst, f.name, arg)
+        
+        return inst
+    
+    def fmt_helper(self, fmt):
+        return '{}({})'.format(
+            self.__class__.__name__,
+            ', '.join('{}={}'.format(f.name, fmt(getattr(self, f.name)))
+                      for f in self._primary_fields))
+    
+    def __str__(self):
+        return self.fmt_helper(str)
+    def __repr__(self):
+        return self.fmt_helper(repr)
+    
+    def __eq__(self, other):
+        if not isinstance(self, other.__class__):
+            return NotImplemented
+        return all(getattr(self, f.name) == getattr(other, f.name)
+                   for f in self._primary_fields)
+    
+    def __neq__(self, other):
+        return not (self == other)
+    
+    def __hash__(self):
+        return hash(tuple(getattr(self, f.name)
+                          for f in self._primary_fields))
---
+++ struct/test_struct.py
@@ -0,0 +1,85 @@
+"""Unit tests for struct.py."""
+
+
+import unittest
+
+from struct.struct import *
+
+
+class StructCase(unittest.TestCase):
+    
+    def testField(self):
+        # Since we're just testing Field and not MetaStruct,
+        # we need to bypass some of the meta machinery and be
+        # more verbose.
+        barfield = Field(int, 'seq')
+        barfield.name = 'bar'
+        class Foo:
+            bar = barfield
+            def __init__(self, b):
+                self._immutable = False
+                self.bar = b
+        
+        # Instantiation.
+        f = Foo([5])
+        self.assertEquals(f.bar, [5])
+        
+        # Type checking.
+        with self.assertRaises(TypeError):
+            f.bar = 'b'
+        
+        # Immutability.
+        f._immutable = True
+        with self.assertRaises(AttributeError):
+            f.bar = [6]
+    
+    def testStruct(self):
+        # Basic functionality.
+        class Foo(Struct):
+            bar = Field(int)
+        f = Foo(5)
+        self.assertEqual(f.bar, 5)
+        
+        # Mutability.
+        class Foo(Struct):
+            _immutable = False
+            bar = Field(int)
+        f = Foo(5)
+        f.bar = 6
+        
+        # Immutability.
+        class Foo(Struct):
+            bar = Field(int)
+            def __init__(self, *_):
+                self.bar += 1
+        f = Foo(5)
+        self.assertEqual(f.bar, 6)
+        with self.assertRaises(AttributeError):
+            f.bar = 7
+        
+        # Pretty-printing.
+        self.assertEqual(str(f), 'Foo(bar=6)')
+        
+        # Derived data.
+        class Foo(Struct):
+            bar = Field(int)
+            baz = Field(int, '!')
+            def __init__(self, bar):
+                self.baz = bar + 1
+        f = Foo(5)
+        self.assertEqual(f.baz, 6)
+        
+        # Equality and hashing.
+        class Foo(Struct):
+            bar = Field(int)
+        f1 = Foo(5)
+        f2 = Foo(5)
+        f3 = Foo(6)
+        self.assertEqual(f1, f2)
+        self.assertNotEqual(f1, f3)
+        self.assertEqual(hash(f1), hash(f2))
+        # hash(f1) == hash(f3) is unlikely but valid.
+
+
+if __name__ == '__main__':
+    unittest.main()
