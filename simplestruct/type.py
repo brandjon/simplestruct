eb11064250ca1ddaa58382366503c926d85bcfc5
Jon Brandvein <jon.brandvein@gmail.com>
2014-12-14 23:49:22 -0500

    Remove check_spec from type.py, tweak comments.

--- simplestruct/type.py
+++ simplestruct/type.py
@@ -1,34 +1,25 @@
-"""Type checking and type coercion."""
+"""Type checking and type coercion.
 
-# Spare me the "It's not the Python way" lectures. I've lost too much
-# time to type errors in places where I never had any intention of
-# allowing duck-typed alternative values.
+Spare me the "It's not the Python way" lectures. I've lost too much
+time to type errors in places where I never had any intention of
+allowing duck-typed alternative values.
 
-# A kind is a tuple of types. A value satisfies a kind if it is an
-# instance of any of the types. For convenience, kinds may also be
-# specified as a single type, a sequence other than a tuple, and as
-# None (equivalent to (object,)).
-
-# A type specification is a kind along with a tuple of modifier strings,
-# which may include:
-#
-#   'seq': sequence of elements satisfying the kind
-#   'nodups': with 'seq', no duplicate elements allowed
-#
-# The modifier strings may also be given as another sequence or as a
-# space-delimited string.
+For our purposes, a "kind" is a tuple of types. A value satisfies
+a kind if it is an instance of any of the types. For convenience,
+kinds may also be specified as a single type, a sequence other than
+a tuple, and as None (equivalent to (object,)).
+"""
 
 
 __all__ = [
     'checktype',
     'checktype_seq',
-    'check_spec',
 ]
 
 
 def str_valtype(val):
     """Get a string describing the type of val."""
-    return type(val).__name__
+    return val.__class__.__name__
 
 def normalize_kind(kind):
     """Make a proper kind out of one of the alternative forms."""
@@ -38,13 +29,6 @@ def normalize_kind(kind):
         return (kind,)
     else:
         return tuple(kind)
-
-def normalize_mods(mods):
-    """Make a modifier list out of space-delimited string."""
-    if isinstance(mods, str):
-        return tuple(mods.split())
-    else:
-        return tuple(mods)
 
 def str_kind(kind):
     """Get a string describing a kind."""
@@ -107,15 +91,3 @@ def checktype_seq(seq, kind, nodups=False):
                 raise TypeError('Duplicate element {} at position {}'.format(
                                 repr(item), i))
             seen.append(item)
-
-
-def check_spec(val, kind, mods):
-    """Raise TypeError if val does not match the type specification
-    given by kind and mods.
-    """
-    mods = normalize_mods(mods)
-    if 'seq' in mods:
-        nodups = 'nodups' in mods
-        checktype_seq(val, kind, nodups=nodups)
-    else:
-        checktype(val, kind)
